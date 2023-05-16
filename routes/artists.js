fbd292a073a404d9c0d0d4b439f745ded5793d89
Guillaume <ronnel.guillaume@proton.me>
2023-05-16 11:08:58 +0200

    fetch artist

--- routes/artists.js
+++ routes/artists.js
@@ -10,11 +10,11 @@ router.get('/:mbid', async (req, res) => {
    .then(response => response.json()).then((artist) => {
       fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist.name}&api_key=${lastapi}&format=json`)
       .then(response => response.json()).then((lastfmartist) => {
-         console.log(lastfmartist)
          const art = {
          ended: artist['life-span'].ended,
          name: artist.name,
-         image: lastfmartist.image[3]['#text']
+         bio: lastfmartist.artist.bio.summary,
+         image: lastfmartist.artist.image[2]['#text']
          }
 
          if (artist !== null) { 
@@ -23,5 +23,60 @@ router.get('/:mbid', async (req, res) => {
       })
    })
 })
+
+router.get('/albums/:mbid', (req, res) => {
+   fetch(`http://musicbrainz.org/ws/2/release-group?artist=${req.params.mbid}&type=album&limit=100&fmt=json`)
+   .then(response => response.json()).then((mbalbums) => {
+      let albums = []
+      mbalbums['release-groups'].map((data, i) => {
+         if (data['secondary-types'].length == 0) {
+            albums.push({
+               release: data['first-release-date'],
+               title: data.title,
+               id: data.id
+            })
+         }
+      })
+      albums.sort(function(a,b){ return new Date(a.release) - new Date(b.release)})
+      res.json({result: true, albums})
+   })
+})
+
+router.get('/eps/:mbid', (req, res) => {
+   fetch(`http://musicbrainz.org/ws/2/release-group?artist=${req.params.mbid}&type=ep&limit=100&fmt=json`)
+   .then(response => response.json()).then((mbeps) => {
+      let eps = []
+      mbeps['release-groups'].map((data, i) => {
+         if (data['secondary-types'].length == 0) {
+            eps.push({
+               release: data['first-release-date'],
+               title: data.title,
+               id: data.id
+            })
+         }
+      })
+      eps.sort(function(a,b){ return new Date(a.release) - new Date(b.release)})
+      res.json({result: true, eps})
+   })
+})
+
+router.get('/singles/:mbid', (req, res) => {
+   fetch(`http://musicbrainz.org/ws/2/release-group?artist=${req.params.mbid}&type=single&limit=100&fmt=json`)
+   .then(response => response.json()).then((mbsingles) => {
+      let singles = []
+      mbsingles['release-groups'].map((data, i) => {
+         if (data['secondary-types'].length == 0 ) {
+            singles.push({
+               release: data['first-release-date'],
+               title: data.title,
+               id: data.id
+            })
+         }
+      })
+      singles.sort(function(a,b){ return new Date(a.release) - new Date(b.release)})
+      res.json({result: true, singles})
+   })
+})
+
 
 module.exports = router;
