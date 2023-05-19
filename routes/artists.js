61f9965d72c604201b87c68ed8c2cced32395580
Guillaume <ronnel.guillaume@proton.me>
2023-05-19 09:55:56 +0200

    anticrash artists

--- routes/artists.js
+++ routes/artists.js
@@ -13,19 +13,24 @@ const url = 'http://musicbrainz.org/ws/2/'
 router.get('/:mbid', (req, res) => {
    fetch(url+`artist/${req.params.mbid}?fmt=json`)
    .then(response => response.json()).then((artist) => {
-      fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist.name}&api_key=${lastapi}&format=json`)
-      .then(response => response.json()).then((lastfmartist) => {
-         const art = {
-         ended: artist['life-span'].ended,
-         name: artist.name,
-         bio: lastfmartist.artist.bio.summary,
-         image: lastfmartist.artist.image[2]['#text']
-         }
-
-         if (artist !== null) { 
-            res.json({ result: true, art })
-         }
-      })
+      if (artist.error) {
+         res.json({ result: false, error: artist.error })
+      } else {
+         fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist.name}&api_key=${lastapi}&format=json`)
+         .then(response => response.json()).then((lastfmartist) => {
+            const art = {
+            ended: artist['life-span'].ended,
+            name: artist.name,
+            bio: lastfmartist.artist.bio.summary,
+            image: lastfmartist.artist.image[2]['#text']
+            }
+   
+            if (artist !== null) { 
+               res.json({ result: true, art })
+            }
+         })
+      }
+      
    })
 })
 
@@ -33,20 +38,23 @@ router.get('/:mbid', (req, res) => {
 router.get('/:mbid/lastalbum', (req, res) => {
    fetch(url+`release-group?artist=${req.params.mbid}&type=album&limit=100&fmt=json`)
    .then(response => response.json()).then((mbalbums) => {
-
-      let album = mbalbums['release-groups'].map((datagroup, i) => {
-         if (datagroup['secondary-types'].length == 0) {
-            return datagroup
-         }
-      })
-      album.sort(function(a,b){ 
-         return new Date(b['first-release-date']) - new Date(a['first-release-date'])
-      })
-      album = album.slice( 0, 1 );
-      fetch(`http://coverartarchive.org/release-group/${album[0].id}?fmt=json`)
-      .then(response => response.json()).then((data) => {
-         res.json({cover : data.images[0].image, date: album[0]['first-release-date'], title: album[0].title})
-      })
+      if (mbalbums.error) {
+         res.json({ result: false, error: mbalbums.error })
+      } else {
+         let album = mbalbums['release-groups'].map((datagroup, i) => {
+            if (datagroup['secondary-types'].length == 0) {
+               return datagroup
+            }
+         })
+         album.sort(function(a,b){ 
+            return new Date(b['first-release-date']) - new Date(a['first-release-date'])
+         })
+         album = album.slice( 0, 1 );
+         fetch(`http://coverartarchive.org/release-group/${album[0].id}?fmt=json`)
+         .then(response => response.json()).then((data) => {
+            res.json({cover : data.images[0].image, date: album[0]['first-release-date'], title: album[0].title})
+         })
+      }
     })
 }) 
 
@@ -97,60 +105,72 @@ router.get('/:mbid/album', (req, res) => {
    
    fetch(url+`release-group?artist=${req.params.mbid}&type=album&limit=100&fmt=json`)
    .then(response => response.json()).then((mbalbums) => {
-      return Promise.all(mbalbums['release-groups'].map((datagroup, i) => {
-         if (datagroup['secondary-types'].length == 0 && datagroup['first-release-date'] !== '') {
-            const album = {
-               title: datagroup.title,
-               mbid: datagroup.id,
-               date: datagroup['first-release-date']
+      if (mbalbums.error) {
+         res.json({ result: false, error: mbalbums.error })
+      } else {
+         return Promise.all(mbalbums['release-groups'].map((datagroup, i) => {
+            if (datagroup['secondary-types'].length == 0 && datagroup['first-release-date'] !== '') {
+               const album = {
+                  title: datagroup.title,
+                  mbid: datagroup.id,
+                  date: datagroup['first-release-date']
+               }
+               return album
             }
-            return album
-         }
-      })).then(data => { 
-         data.sort(function(a,b){ return new Date(a.date) - new Date(b.date)})
-         data = data.filter( Boolean ); // removes undefined
-         res.json({result: true, releases: data})
-      })
+         })).then(data => { 
+            data.sort(function(a,b){ return new Date(a.date) - new Date(b.date)})
+            data = data.filter( Boolean ); // removes undefined
+            res.json({result: true, releases: data})
+         })
+      }
    })
 }) 
 
 router.get('/:mbid/ep', (req, res) => {
    fetch(url+`release-group?artist=${req.params.mbid}&type=ep&limit=100&fmt=json`)
    .then(response => response.json()).then((mbalbums) => {
-      return Promise.all(mbalbums['release-groups'].map((datagroup, i) => {
-         if (datagroup['secondary-types'].length == 0 && datagroup['first-release-date'] !== '') {
-            const album = {
-               title: datagroup.title,
-               mbid: datagroup.id,
-               date: datagroup['first-release-date']
+      if (mbalbums.error) {
+         res.json({ result: false, error: mbalbums.error })
+      } else {
+         return Promise.all(mbalbums['release-groups'].map((datagroup, i) => {
+            if (datagroup['secondary-types'].length == 0 && datagroup['first-release-date'] !== '') {
+               const album = {
+                  title: datagroup.title,
+                  mbid: datagroup.id,
+                  date: datagroup['first-release-date']
+               }
+               return album
             }
-            return album
-         }
-      })).then(data => { 
-         data.sort(function(a,b){ return new Date(a.date) - new Date(b.date)})
-         data = data.filter( Boolean ); // removes undefined
-         res.json({result: true, releases: data})
-      })
+         })).then(data => { 
+            data.sort(function(a,b){ return new Date(a.date) - new Date(b.date)})
+            data = data.filter( Boolean ); // removes undefined
+            res.json({result: true, releases: data})
+         })
+      }
    })
 })
 
 router.get('/:mbid/single', (req, res) => {
    fetch(url+`release-group?artist=${req.params.mbid}&type=single&limit=100&fmt=json`)
    .then(response => response.json()).then((mbalbums) => {
-      return Promise.all(mbalbums['release-groups'].map((datagroup, i) => {
-         if (datagroup['secondary-types'].length == 0 && datagroup['first-release-date'] !== '') {
-            const album = {
-               title: datagroup.title,
-               mbid: datagroup.id,
-               date: datagroup['first-release-date']
+      if (mbalbums.error) {
+         res.json({ result: false, error: mbalbums.error })
+      } else {
+         return Promise.all(mbalbums['release-groups'].map((datagroup, i) => {
+            if (datagroup['secondary-types'].length == 0 && datagroup['first-release-date'] !== '') {
+               const album = {
+                  title: datagroup.title,
+                  mbid: datagroup.id,
+                  date: datagroup['first-release-date']
+               }
+               return album
             }
-            return album
-         }
-      })).then(data => { 
-         data.sort(function(a,b){ return new Date(a.date) - new Date(b.date)})
-         data = data.filter( Boolean ); // removes undefined
-         res.json({result: true, releases: data})
-      })
+         })).then(data => { 
+            data.sort(function(a,b){ return new Date(a.date) - new Date(b.date)})
+            data = data.filter( Boolean ); // removes undefined
+            res.json({result: true, releases: data})
+         })
+      }
    })
 })
 
@@ -158,43 +178,44 @@ router.get('/:mbid/single', (req, res) => {
 router.post('/', (req, res) => {
    fetch(url+`artist/${req.body.mbid}?fmt=json`)
    .then(response => response.json()).then((artist) => {
-      Artist.findOne({ mbid: req.body.mbid }).then((dbartist) => {
-         if (dbartist == null) {
-            const newArtist = new Artist({ mbid: req.body.mbid, name: artist.name})
-
-            newArtist.save().then((newdbartist) => {
+      if (artist.error) {
+         res.json({ result: false, error: artist.error })
+      } else {
+         Artist.findOne({ mbid: req.body.mbid }).then((dbartist) => {
+            if (dbartist == null) {
+               const newArtist = new Artist({ mbid: req.body.mbid, name: artist.name})
+   
+               newArtist.save().then((newdbartist) => {
+                  User.findOne({ token: req.body.token }).then((user) => {
+                     Profile.updateOne({ user: user.id }, {
+                        $push: { artists: newdbartist.id }
+                     }).then((profile) => {
+                        res.json({ result: true, profile })
+                     })
+                  })
+               })
+            } else {
                User.findOne({ token: req.body.token }).then((user) => {
                   Profile.updateOne({ user: user.id }, {
-                     $push: { artists: newdbartist.id }
+                     $addToSet: { artists: dbartist.id }
                   }).then((profile) => {
                      res.json({ result: true, profile })
                   })
                })
-            })
-         } else {
-            User.findOne({ token: req.body.token }).then((user) => {
-               Profile.updateOne({ user: user.id }, {
-                  $addToSet: { artists: dbartist.id }
-               }).then((profile) => {
-                  res.json({ result: true, profile })
-               })
-            })
-         }
-      })
+            }
+         })
+      }
    })
 })
 
 /* unfollow an artist */
 router.delete('/', (req, res) => {
-   fetch(url+`artist/${req.body.mbid}?fmt=json`)
-   .then(response => response.json()).then((artist) => {
-      Artist.findOne({ mbid: req.body.mbid }).then((dbartist) => {
-         User.findOne({ token: req.body.token }).then((user) => {
-            Profile.updateOne({ user: user.id }, {
-               $pull: { artists: dbartist.id }
-            }).then((profile) => {
-               res.json({ result: true, profile })
-            })
+   Artist.findOne({ mbid: req.body.mbid }).then((dbartist) => {
+      User.findOne({ token: req.body.token }).then((user) => {
+         Profile.updateOne({ user: user.id }, {
+            $pull: { artists: dbartist.id }
+         }).then((profile) => {
+            res.json({ result: true, profile })
          })
       })
    })
