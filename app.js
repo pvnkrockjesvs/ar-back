13d83b50d57a8dbe52716cc69b8d056290fc6cd0
Roger <roger.nguyen@hotmail.fr>
2023-05-23 17:16:56 +0200

    new route streaming, with spotify

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-require('dotenv').config();
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var profilesRouter = require('./routes/profiles');
-var artistsRouter = require('./routes/artists');
-var releasesRouter = require('./routes/releases');
-var topreleasesRouter = require('./routes/topreleases');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+require("dotenv").config();
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var profilesRouter = require("./routes/profiles");
+var artistsRouter = require("./routes/artists");
+var releasesRouter = require("./routes/releases");
+var topreleasesRouter = require("./routes/topreleases");
+var streamingRouter = require("./routes/streaming");
 
 var app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
-app.use('/artists', artistsRouter);
-app.use('/releases', releasesRouter);
-app.use('/topreleases', topreleasesRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/profiles", profilesRouter);
+app.use("/artists", artistsRouter);
+app.use("/releases", releasesRouter);
+app.use("/topreleases", topreleasesRouter);
+app.use("/streaming", streamingRouter);
 
 module.exports = app;
