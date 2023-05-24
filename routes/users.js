0918ce6330b93a801dc0ededeafd03897f6e323c
Benzeghiba Mohammed Faouzi <bigmfb@gmail.com>
2023-05-24 11:12:20 +0200

    add email notification

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 require("../models/connection");
+const nodemailer = require("nodemailer");
 
 var router = express.Router();
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 const { checkBody } = require("../modules/checkBody");
 const User = require("../models/users");
-
 
 router.post('/signup', (req, res) => {
   if (!checkBody(req.body, ["username", "password", "email"])) {
@@ -62,6 +62,64 @@ router.post('/update', (req, res) => {
     password: hash,
   }).then((user) => {
     res.json({ result: true, token: user.token, username: user.username })
+  })
+})
+
+router.post('/signupconfirm', (req, res) => {
+  if (!checkBody(req.body, ["username", "password", "email"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  console.log
+  User.findOne({ username: req.body.username }).then((user) => {
+    if (user === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        token: uid2(32)
+      })
+
+      newUser.save().then((userInfo) => {
+        // send verification email
+        console.log('CONFIRMARTION:')
+        const transporter = nodemailer.createTransport({
+          host: process.env.HOST,
+          port: process.env.PORT,
+          secure: process.env.SECURE,
+          auth: {
+            user: process.env.USER_NAME,
+            pass: process.env.USER_PASSWORD
+          }
+        })
+        let confirmationCode = userInfo.token
+        console.log('CONFIRMARTION:', confirmationCode)
+        let mailOptions = {
+          from: 'myemail4@ethereal.com',
+          to: userInfo.email,
+          subject: 'Subscription Confirmation Link',
+          html: `<div>
+                <h1>Email Confirmation</h1>
+                <h2>Hello ${userInfo.username}</h2>
+                <p>Thank you for subscribing. Please verify your email account by clicking on the following link</p>
+                <a href=http://${req.headers.host}/confirm/${confirmationCode}> Click here</a>
+                </div>`,
+        }
+        console.log('CONFIRMARTION:333', mailOptions.html)
+        transporter.sendMail(mailOptions, function (err) {
+          if (err) {
+            res.json({ result: false, error: err});
+          } else {
+            res.json({ result: true, token: userInfo.token, username: userInfo.username, message: `Confirmation email sent to ${userInfo.email}` })
+          }
+        })
+      })
+    } else {
+      res.json({ result: false, error: "User already in db" });
+    }
   })
 })
 
